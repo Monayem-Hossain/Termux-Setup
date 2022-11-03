b4f77e531dcf2470d9a1c218e081e8122f827d26
Monayem Hossain 111 <74169836+Devil-King-Monayem@users.noreply.github.com>
2022-11-03 18:49:06 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,223 @@
+echo 
+echo
+
+
+echo "Wait For All Package Download."
+
+echo
+echo
+echo
+
+
+apt update
+
+clear
+
+apt upgrade
+
+clear
+
+termux-setup-storage
+
+clear
+
+pkg install python -y
+
+clear
+
+pkg install python2 -y
+
+clear
+
+pkg install python-static -y
+
+clear
+
+pkg install python-tkinter -y
+
+clear
+pkg install vim-python -y
+
+clear
+
+pkg install python2-static -y
+
+clear
+
+pkg install weechat-python-plugin -y
+
+clear
+
+pkg install git -y
+
+clear
+
+pkg install figlet -y
+
+clear
+
+pkg install cmatrix -y
+
+
+
+clear
+
+pkg install toilet -y
+
+clear
+
+pkg install nano -y
+
+clear
+
+pkg install php -y
+
+clear
+
+pkg install fish -y
+
+clear
+
+pip install astroid 
+
+clear
+
+pip install autopep8
+
+clear
+
+pip install certifi
+
+clear
+
+pip install chardet
+
+clear
+
+pip install colorama 
+
+clear
+
+pip install future
+
+clear
+
+pip install idna 
+
+clear
+
+pip install isort 
+
+clear
+
+pip install lazy-object-proxy
+
+clear
+
+pip install lolcat 
+
+clear
+
+pip install mccabe  
+
+clear
+
+pip install pilo 
+
+clear
+
+pip install pip
+
+clear
+
+pip install pycodestyle 
+
+clear
+
+pip install pyfiglet
+
+clear
+
+pip install pylint
+
+clear
+
+pip install requests 
+
+clear
+
+pip install setuptools
+
+clear
+
+pip install six
+
+clear
+
+pip install termcolor
+
+clear
+
+pip install toml
+
+clear
+
+pip install urllib3
+
+clear
+
+pip install wheel
+
+clear
+
+pip install wrapt
+
+clear
+
+pip install youtube-dl
+
+clear
+
+pip install mechanize 
+
+clear
+
+apt install ruby -y
+
+clear
+
+apt install openssh -y
+
+clear
+
+apt install wget -y
+
+clear
+
+apt install curl -y
+
+clear
+
+apt install proot -y
+
+clear
+
+figlet -f slant 'Devil-King' |lolcat
+figlet -f slant 'Monayem' |lolcat
+echo
+sleep 1
+sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=>' ; sleep 0.1; echo -n '=> X' ; sleep 0.1;
+sleep 3
+
+echo
+clear
+echo 
+figlet -f slant 'WELLCOME' |lolcat
+echo
+echo
+sleep 2
+echo
+
+echo "Now Your Termux Is Ready To Run," |lolcat
+
+#Remake By Mao2116
